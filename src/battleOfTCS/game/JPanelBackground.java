f4ee1a6a7ecd9831821c5dd153e1128bae0d6f3d
olamedrek <olamed@op.pl>
2015-06-04 22:57:11 +0200

    New background

--- src/battleOfTCS/game/JPanelBackground.java
+++ src/battleOfTCS/game/JPanelBackground.java
@@ -15,15 +15,21 @@ public class JPanelBackground extends JPanel {
 	static Image backGround = new ImageIcon(Controller.class.getResource("images/backbroundmenuv3.jpg")).getImage();
 	static Image backGroundOptions = new ImageIcon(Controller.class.getResource("images/backbroundmenuv2.jpg")).getImage();
 	static Image backGroundGame = new ImageIcon(Controller.class.getResource("images/background.png")).getImage();
+	static Image newBackGround = new ImageIcon(Controller.class.getResource("images/newbackground.png")).getImage();
+	static Image newBackGround2 = new ImageIcon(Controller.class.getResource("images/newbackground2.png")).getImage();
+	static Image battle = new ImageIcon(Controller.class.getResource("images/battle.png")).getImage();
 
 	
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		if (backGround != null) {
-			if (mode == 1)
-				g.drawImage(backGround, 0, 0, this.getWidth(), this.getHeight(), this);
+			if (mode == 1) {
+				g.drawImage(newBackGround, 0, 0, this);
+				g.drawImage(battle, (this.getWidth() - battle.getWidth(null))/2, 40, this);
+			}
+				
 			if (mode == 2)
-				g.drawImage(backGroundOptions, 0, 0, this.getWidth(), this.getHeight(), this);
+				g.drawImage(newBackGround, 0, 0, this);
 			if (mode == 3) {
 				g.drawImage(backGroundGame, 0, 0, this.getWidth(), this.getHeight(), this);
 				for (HexMapElement hex : controller.game.map.hexes)
